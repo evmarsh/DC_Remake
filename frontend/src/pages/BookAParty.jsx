f72219f14585744bc10884e876d476e7c8f082fe
Evan Marsh <evanmarsh2003@gmail.com>
2025-06-28 14:55:13 -0500

    Fixed minor bug where content wouldn't display

---
+++ frontend/src/pages/BookAParty.jsx
@@ -0,0 +1,9 @@
+function BookAParty () {
+    return (
+        <div className="min-h-full min-w-full bg-light items-center justify-center flex flex-col">
+            <p>Test</p>
+        </div>
+    );
+}
+
+export default BookAParty
