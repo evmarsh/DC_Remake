6b06bf6e02ff5036421a000585f9ae3c935e4eae
Evan Marsh <evanmarsh2003@gmail.com>
2025-06-28 16:02:39 -0500

    Added background to book party

--- frontend/src/pages/BookAParty.jsx
+++ frontend/src/pages/BookAParty.jsx
@@ -2,7 +2,7 @@ import Footer from "../components/Footer";
 
 function BookAParty () {
     return (
-        <div className="w-full flex flex-col items-center justify-center relative overflow-hidden">
+        <div className="min-w-screen bg-light flex flex-col items-center justify-center relative overflow-hidden">
             <form className="border-gray-500 border-2 shadow-2xs bg-light w-1/2 h-dvh flex flex-col items-center justify-center text-black rounded">
                 <input type="text" name="firstName" placeholder="First Name" className="border-black border-2 w-3/4 h-10 rounded-xl p-2" />
             </form>
