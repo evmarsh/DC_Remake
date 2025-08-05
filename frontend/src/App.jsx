7c4ea72b789eae49f63dcda9e4adc34dc7036542
Evan Marsh <evanmarsh2003@gmail.com>
2025-08-05 18:52:42 -0500

    Fixed conditional rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,27 +8,19 @@ import AppLayout from './components/AppLayout'
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/*" element={
-          <AppLayout layoutType="default" className="main-content flex flex-col min-h-screen bg-transparent w-full pt-16 flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/book_a_party" element={<BookAParty />} />
-              <Route path="/menu" element={<Menu />} />
-            </Routes>
-          </AppLayout>
-        } 
-        />
-        <Route path="/admin/*" element={
-          <AppLayout layoutType="admin" className="main-content flex flex-col min-h-screen bg-transparent w-full pt-16 flex-grow">
-            <Routes>
-              <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-          </AppLayout>
-        } />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<AppLayout layoutType="default" className="main-content flex flex-col min-h-screen bg-transparent w-full pt-16 flex-grow" />}>
+        <Route index element={<Home />} />
+        <Route path="book_a_party" element={<BookAParty />} />
+        <Route path="menu" element={<Menu />} />
+      </Route>
+
+      <Route path="/admin" element={<AppLayout layoutType="admin" className="main-content flex flex-col min-h-screen bg-transparent w-full pt-16 flex-grow" />}>
+          <Route index element={<Dashboard />} />
+      </Route>
+
+      <Route path="*" element={<div className="text-center text-red-500 justify-center">404 Not Found</div>} />
+    </Routes>
   );
 };
 
--- frontend/src/components/AppLayout.jsx
+++ frontend/src/components/AppLayout.jsx
@@ -1,11 +1,15 @@
+import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import SideBar from './SideBar';
+import Footer from './Footer';
 
-const AppLayout = ({ layoutType, children, className }) => {
+const AppLayout = ({ layoutType, className }) => {
     return (
         <div>
             {layoutType === 'admin' ? <SideBar /> : <Navbar />}
-            <main className={className}>{children}</main>
+            <main className={className}>
+                <Outlet />
+            </main>
             {layoutType !== 'admin' ? <Footer /> : null}
         </div>
     );
